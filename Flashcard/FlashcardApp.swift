bd67721a4af394a65141f8607853a22f644d5a15
Gikai Andrews <101468353+GikaiA@users.noreply.github.com>
2024-10-09 16:21:34 -0400

    Initial Commit

---
+++ Flashcard/FlashcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardApp.swift
+//  Flashcard
+//
+//  Created by Gikai Andrews on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
